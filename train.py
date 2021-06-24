294804fe86ed3d8bf8df6760a46e9f76f87eff6c
夜雨飘零 <yeyupiaoling@foxmail.com>
2021-06-24 18:03:13 +0800

    修复模型不收敛

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ import functools
 import os
 import re
 import shutil
-from datetime import datetime
+import time
+from datetime import datetime, timedelta
 
 import paddle
 import paddle.distributed as dist
@@ -24,12 +25,12 @@ add_arg = functools.partial(add_arguments, argparser=parser)
 add_arg('gpus',             str,    '0',                      '训练使用的GPU序号，使用英文逗号,隔开，如：0,1')
 add_arg('batch_size',       int,    64,                       '训练的批量大小')
 add_arg('num_workers',      int,    4,                        '读取数据的线程数量')
-add_arg('num_epoch',        int,    100,                      '训练的轮数')
+add_arg('num_epoch',        int,    50,                       '训练的轮数')
 add_arg('learning_rate',    float,  1e-1,                     '初始学习率的大小')
-add_arg('use_model',        str,    'mobilefacenet',          '所使用的模型，支持mobilefacenet，resnet_face34')
+add_arg('use_model',        str,    'mobilefacenet',          '所使用的模型，支持 mobilefacenet，resnet_face34')
 add_arg('train_root_path',  str,    'dataset/images',         '训练数据的根目录')
 add_arg('test_list_path',   str,    'dataset/lfw_test.txt',   '测试数据的数据列表路径')
-add_arg('save_model',       str,    'models/mobilefacenet',   '模型保存的路径')
+add_arg('save_model',       str,    'models/',                '模型保存的路径')
 add_arg('resume',           str,    None,                     '恢复训练，当为None则不使用恢复模型')
 add_arg('pretrained_model', str,    None,                     '预训练模型的路径，当为None则不使用预训练模型')
 args = parser.parse_args()
@@ -50,7 +51,7 @@ def test(model):
 
 # 保存模型
 def save_model(args, epoch, model, metric_fc, optimizer):
-    model_params_path = os.path.join(args.save_model, 'params', 'epoch_%d' % epoch)
+    model_params_path = os.path.join(args.save_model, args.use_model, 'params', 'epoch_%d' % epoch)
     if not os.path.exists(model_params_path):
         os.makedirs(model_params_path)
     # 保存模型参数
@@ -58,14 +59,14 @@ def save_model(args, epoch, model, metric_fc, optimizer):
     paddle.save(metric_fc.state_dict(), os.path.join(model_params_path, 'metric_fc.pdparams'))
     paddle.save(optimizer.state_dict(), os.path.join(model_params_path, 'optimizer.pdopt'))
     # 删除旧的模型
-    old_model_path = os.path.join(args.save_model, 'params', 'epoch_%d' % (epoch - 3))
+    old_model_path = os.path.join(args.save_model, args.use_model, 'params', 'epoch_%d' % (epoch - 3))
     if os.path.exists(old_model_path):
         shutil.rmtree(old_model_path)
     # 保存预测模型
-    if not os.path.exists(os.path.join(args.save_model, 'infer')):
-        os.makedirs(os.path.join(args.save_model, 'infer'))
+    if not os.path.exists(os.path.join(args.save_model, args.use_model, 'infer')):
+        os.makedirs(os.path.join(args.save_model, args.use_model, 'infer'))
     paddle.jit.save(layer=model,
-                    path=os.path.join(args.save_model, 'infer/model'),
+                    path=os.path.join(args.save_model, args.use_model, 'infer/model'),
                     input_spec=[InputSpec(shape=[None, 3, 112, 112], dtype=paddle.float32)])
 
 
@@ -85,6 +86,7 @@ def train(args):
     else:
         batch_sampler = paddle.io.BatchSampler(train_dataset, batch_size=args.batch_size, shuffle=True)
     train_loader = DataLoader(dataset=train_dataset, batch_sampler=batch_sampler, num_workers=args.num_workers)
+    print("[%s] 总数据类别为：%d" % (datetime.now(), train_dataset.num_classes))
 
     # 获取模型，贴心的作者同时提供了resnet的模型，以满足不同情况的使用
     if args.use_model == 'resnet_face34':
@@ -103,11 +105,12 @@ def train(args):
     # 获取预训练的epoch数
     last_epoch = int(re.findall(r'\d+', args.resume)[-1]) + 1 if args.resume is not None else 0
     # 学习率衰减
-    scheduler = paddle.optimizer.lr.StepDecay(learning_rate=args.learning_rate, step_size=1, gamma=0.83, last_epoch=last_epoch, verbose=True)
+    scheduler = paddle.optimizer.lr.StepDecay(learning_rate=args.learning_rate, step_size=10, gamma=0.1, last_epoch=last_epoch, verbose=True)
     # 设置优化方法
-    optimizer = paddle.optimizer.Adam(parameters=model.parameters() + metric_fc.parameters(),
-                                      learning_rate=scheduler,
-                                      weight_decay=paddle.regularizer.L2Decay(5e-4))
+    optimizer = paddle.optimizer.Momentum(parameters=model.parameters() + metric_fc.parameters(),
+                                          learning_rate=scheduler,
+                                          momentum=0.9,
+                                          weight_decay=paddle.regularizer.L2Decay(5e-4))
 
     # 加载预训练模型
     if args.pretrained_model is not None:
@@ -123,29 +126,30 @@ def train(args):
             else:
                 print('Lack weight: {}'.format(name))
         model.set_dict(model_state_dict)
-        print('成功加载 model 参数')
+        print('[%s] 成功加载 model 参数' % datetime.now())
 
     # 恢复训练
     if args.resume is not None:
         model.set_state_dict(paddle.load(os.path.join(args.resume, 'model.pdparams')))
         metric_fc.set_state_dict(paddle.load(os.path.join(args.resume, 'metric_fc.pdparams')))
         optimizer.set_state_dict(paddle.load(os.path.join(args.resume, 'optimizer.pdopt')))
-        print('成功加载模型参数和优化方法参数')
+        print('[%s] 成功加载模型参数和优化方法参数' % datetime.now())
 
     # 获取损失函数
     loss = paddle.nn.CrossEntropyLoss()
     train_step = 0
     test_step = 0
+    sum_batch = len(train_loader) * (args.num_epoch - last_epoch)
     # 开始训练
     for epoch in range(last_epoch, args.num_epoch):
         loss_sum = []
         accuracies = []
         for batch_id, (img, label) in enumerate(train_loader()):
+            start = time.time()
             feature = model(img)
             output = metric_fc(feature, label)
             # 计算损失值
             los = loss(output, label)
-            loss_sum.append(los.numpy()[0])
             los.backward()
             optimizer.step()
             optimizer.clear_grad()
@@ -153,23 +157,29 @@ def train(args):
             label = paddle.reshape(label, shape=(-1, 1))
             acc = accuracy(input=output, label=label)
             accuracies.append(acc.numpy()[0])
+            loss_sum.append(los.numpy()[0])
             # 多卡训练只使用一个进程打印
             if batch_id % 100 == 0 and dist.get_rank() == 0:
-                print('[%s] Train epoch %d, batch_id: %d, loss: %f, accuracy: %f' % (
-                    datetime.now(), epoch, batch_id, sum(loss_sum) / len(loss_sum), sum(accuracies) / len(accuracies)))
+                eta_sec = ((time.time() - start) * 1000) * (sum_batch - (epoch - last_epoch) * len(train_loader) - batch_id)
+                eta_str = str(timedelta(seconds=int(eta_sec / 1000)))
+                print('[%s] Train epoch %d, batch: %d/%d, loss: %f, accuracy: %f, eta: %s' % (
+                    datetime.now(), epoch, batch_id, len(train_loader), sum(loss_sum) / len(loss_sum), sum(accuracies) / len(accuracies), eta_str))
                 writer.add_scalar('Train loss', los, train_step)
                 train_step += 1
                 loss_sum = []
         # 多卡训练只使用一个进程执行评估和保存模型
         if dist.get_rank() == 0:
+            print('='*70)
             acc = test(model)
             print('[%s] Test %d, accuracy: %f' % (datetime.now(), epoch, acc))
+            print('='*70)
             writer.add_scalar('Test acc', acc, test_step)
             # 记录学习率
             writer.add_scalar('Learning rate', scheduler.last_lr, epoch)
             test_step += 1
             save_model(args, epoch, model, metric_fc, optimizer)
         scheduler.step()
+    save_model(args, args.num_epoch, model, metric_fc, optimizer)
 
 
 if __name__ == '__main__':
